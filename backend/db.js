da1365821f67522ea64fb0dad735a534c388728f
Jing1224 <JingPro1224@gmail.com>
2025-11-18 21:03:32 -0500

    add SSL configuration for Supabase connection

--- backend/db.js
+++ backend/db.js
@@ -8,6 +8,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 //Test database connection
 pool.connect((err, _client, release) => {
